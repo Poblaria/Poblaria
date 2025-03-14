b3193ad1423c9c404c97f13f7f53b061e081eb55
Clement-Z4RM <clement.lienard@epitech.eu>
2025-03-14 18:45:45 +0100

    feat: add authentification routes
    Add register, login and logout routes.
    Also make access tokens expire after 30 days.

---
+++ backend/app/controllers/auth_controller.ts
@@ -0,0 +1,24 @@
+import type { HttpContext } from "@adonisjs/core/http";
+import { loginValidator, registerValidator } from "#validators/auth";
+import User from "#models/user";
+
+export default class AuthController {
+    async register({ request, response }: HttpContext) {
+        return response.created(await User.create(await request.validateUsing(registerValidator)));
+    }
+
+    async login({ request, response }: HttpContext) {
+        const { email, password } = await request.validateUsing(loginValidator);
+        const user = await User.verifyCredentials(email, password);
+
+        return response.ok(await User.accessTokens.create(user));
+    }
+
+    async logout({ auth, response }: HttpContext) {
+        const user = auth.getUserOrFail();
+        const token = user.currentAccessToken.identifier;
+
+        await User.accessTokens.delete(user, token);
+        return response.ok("");
+    }
+}
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,9 +8,14 @@
 */
 
 import router from "@adonisjs/core/services/router";
+import { middleware } from "#start/kernel";
 
-router.get("/", async () => {
-    return {
-        hello: "world"
-    };
-});
+const AuthController = () => import("#controllers/auth_controller");
+
+router
+    .group(() => {
+        router.post("register", [AuthController, "register"]).as("register");
+        router.post("/login", [AuthController, "login"]).as("login");
+        router.post("logout", [AuthController, "logout"]).use(middleware.auth()).as("logout");
+    })
+    .as("auth");
