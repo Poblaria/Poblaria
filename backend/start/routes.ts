22bc67efe0e3674aa5ac41f452021841fbe30368
Clement-Z4RM <clement.lienard@epitech.eu>
2025-03-18 16:37:11 +0100

    feat(backend): add "/" route with some API information and endpoints list

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -14,6 +14,14 @@ const AuthController = () => import("#controllers/auth_controller");
 const HousingController = () => import("#controllers/housing_controller");
 const JobController = () => import("#controllers/job_controller");
 
+router.get("/", async () => ({
+    name: "Poblaria API",
+    version: "0.0.1",
+    description:
+        "Welcome to Poblaria API. Use the provided endpoints to interact with the service.",
+    endpoints: router.toJSON()["root"].map((route) => `${route.methods[0]} ${route.pattern}`)
+}));
+
 router
     .group(() => {
         router.post("register", [AuthController, "register"]).as("register");
