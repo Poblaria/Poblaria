e31ffbdd4806b9903e7cbbc3d254f49b1285d32a
Clement-Z4RM <clement.lienard@epitech.eu>
2025-09-28 00:15:20 -0400

    refactor(backend-seeders): re-order `MainSeeder` class methods

--- backend/database/seeders/main/main_seeder.ts
+++ backend/database/seeders/main/main_seeder.ts
@@ -2,21 +2,6 @@ import { BaseSeeder } from "@adonisjs/lucid/seeders";
 import app from "@adonisjs/core/services/app";
 
 export default class MainSeeder extends BaseSeeder {
-    private async seed(Seeder: { default: typeof BaseSeeder }) {
-        /**
-         * Do not run when not in an environment specified in Seeder
-         */
-        if (
-            Seeder.default.environment &&
-            ((!Seeder.default.environment.includes("development") && app.inDev) ||
-                (!Seeder.default.environment.includes("testing") && app.inTest) ||
-                (!Seeder.default.environment.includes("production") && app.inProduction))
-        )
-            return;
-
-        await new Seeder.default(this.client).run();
-    }
-
     async run() {
         /**
          * Housing seeders
@@ -31,5 +16,20 @@ export default class MainSeeder extends BaseSeeder {
         await this.seed(await import("#database/seeders/job_type_seeder"));
         await this.seed(await import("#database/seeders/job_industry_seeder"));
         await this.seed(await import("#database/seeders/job_seeder"));
+    }
+
+    private async seed(Seeder: { default: typeof BaseSeeder }) {
+        /**
+         * Do not run when not in an environment specified in Seeder
+         */
+        if (
+            Seeder.default.environment &&
+            ((!Seeder.default.environment.includes("development") && app.inDev) ||
+                (!Seeder.default.environment.includes("testing") && app.inTest) ||
+                (!Seeder.default.environment.includes("production") && app.inProduction))
+        )
+            return;
+
+        await new Seeder.default(this.client).run();
     }
 }
