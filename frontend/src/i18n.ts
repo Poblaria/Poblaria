400ed9335dba07fb7c2a9d08374f2fc809591025
Franceska Rrokaj <frrrokaj@emeal.nttdata.com>
2025-10-18 23:23:51 +0200

    fix: path of locals files

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "../locals/en.json";
-import es from "../locals/es.json";
+import en from "./locals/en.json";
+import es from "./locals/es.json";
 
 void i18n
   .use(initReactI18next)
