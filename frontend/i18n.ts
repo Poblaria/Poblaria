3c49e9849934a38f192f411875fbca7665e043a3
Franceska Rrokaj <frrrokaj@emeal.nttdata.com>
2025-10-18 23:01:04 +0200

    add: i18n files for english/spanish language

---
+++ frontend/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locals/en.json";
+import es from "./locals/es.json";
+
+void i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
