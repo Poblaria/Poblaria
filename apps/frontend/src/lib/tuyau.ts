36a03141356e0acb898b8e1f7ae7608c7505fbf6
Clement-Z4RM <clement.lienard@epitech.eu>
2025-11-06 02:21:43 -0500

    feat(frontend-lib): add authorization token handling in Tuyau setup

--- apps/frontend/src/lib/tuyau.ts
+++ apps/frontend/src/lib/tuyau.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import { createTuyau } from "@tuyau/client";
 import { api } from "@poblaria/backend/api";
 
@@ -6,5 +7,14 @@ if (!process.env.API_BASE_URL || typeof process.env.API_BASE_URL !== "string")
 
 export const tuyau = createTuyau({
     api,
-    baseUrl: process.env.API_BASE_URL
+    baseUrl: process.env.API_BASE_URL,
+    hooks: {
+        beforeRequest: [
+            async (request) => {
+                const token = (await cookies()).get("token")?.value;
+                if (token)
+                    request.headers.set("Authorization", `Bearer ${token}`);
+            }
+        ]
+    }
 });
