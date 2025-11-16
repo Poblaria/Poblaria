da3229038020d13d911dfe8de763cf8bead98ac4
Elisenda <elisenda@6867f322-35ba-4bce-b9a0-d9529bc5fa58.fritz.box>
2025-11-16 20:39:21 +0100

    fix(frontend) fixing lint

--- apps/frontend/src/app/explore/data/getHousingById.ts
+++ apps/frontend/src/app/explore/data/getHousingById.ts
@@ -1,7 +1,9 @@
 import { HOUSES } from "@/app/explore/data/Data";
 
 export function getHousingById(id: string) {
-  return HOUSES.find(h => {
-    return String(h.id) === String(id);
-  }) ?? null;
+    return (
+        HOUSES.find((h) => {
+            return String(h.id) === String(id);
+        }) ?? null
+    );
 }
