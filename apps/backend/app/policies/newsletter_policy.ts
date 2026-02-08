bd498e5b62a30e6eda1d2846c604f2eaf35f613a
Clement-Z4RM <clement.lienard@epitech.eu>
2026-02-07 20:04:32 -0500

    feat(backend): update policies to restrict some actions to specific roles

--- apps/backend/app/policies/newsletter_policy.ts
+++ apps/backend/app/policies/newsletter_policy.ts
@@ -1,12 +1,13 @@
 import { BasePolicy } from "@adonisjs/bouncer";
 import type { AuthorizerResponse } from "@adonisjs/bouncer/types";
+import User from "#models/user";
 
 export default class NewsletterPolicy extends BasePolicy {
-    index(): AuthorizerResponse {
-        return true; // TODO: based on user role - only admins
+    index(user: User): AuthorizerResponse {
+        return user.role === "administrator";
     }
 
-    send(): AuthorizerResponse {
-        return true; // TODO: based on user role - only admins
+    send(user: User): AuthorizerResponse {
+        return user.role === "administrator";
     }
 }
