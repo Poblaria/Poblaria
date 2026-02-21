2bf84a4516ee4bd21f5c32ca55d5eda6320c0651
Elisenda <elisenda@Mac.fritz.box>
2026-02-21 22:08:43 +0100

    fix: add the footer to the layout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import I18nInit from "@/components/I18nInit";
 import { NavBar } from "@/components/NavBar";
-import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
 import Footer from "@/components/Footer";
+import "./globals.css";
 
 export const metadata: Metadata = {
     title: "Poblaria",
@@ -14,10 +15,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <body>
-                <I18nInit />
-                <NavBar />
-                {children}
-                <Footer />
+                <AuthProvider>
+                    <I18nInit />
+                    <NavBar />
+                    {children}
+                    <Footer />
+                </AuthProvider>
             </body>
         </html>
     );
