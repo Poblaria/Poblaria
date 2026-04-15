4b5cf8033e47362066a70f8e997e2d878ac10e2e
josephine <josephine.chicouras@epitech.eu>
2026-04-15 14:26:33 +0200

    feat: add job creation form and confirmation modal

--- apps/frontend/src/app/organizations/job/components/JobForm.tsx
+++
@@ -1,7 +0,0 @@
-export default function JobForm() {
-    return (
-        <div>
-            <h1>Job Page</h1>
-        </div>
-    );
-}
--- apps/frontend/src/app/organizations/job/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function JobPage() {
-    return (
-        <div>
-            <h1>Job Page</h1>
-        </div>
-    );
-}
