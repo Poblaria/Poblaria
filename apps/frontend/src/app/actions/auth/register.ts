dd1cb2e2f6d91d75a43def168ac1b7e8ed166638
Clement-Z4RM <clement.lienard@epitech.eu>
2026-04-27 12:42:33 -0400

    feat(auth): return token on register

--- apps/frontend/src/app/actions/auth/register.ts
+++ apps/frontend/src/app/actions/auth/register.ts
@@ -1,11 +1,15 @@
 "use server";
 import type { InferRequestType, InferResponseType } from "@tuyau/client";
 import { tuyau } from "@lib/tuyau";
+import setTokenCookie from "@actions/auth/utils/setTokenCookie";
 
 export default async function register(
     body: InferRequestType<typeof tuyau.register.$post>
 ) {
     const { data, error } = await tuyau.register.$post(body);
+
+    if (data?.token) await setTokenCookie(data.token, data.expiresAt);
+
     return { data, error: error?.value };
 }
 
